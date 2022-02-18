dbacb6e4c8aeb3a65c065896fd9a59c5c7db6633
Felipe Castro Ferreira <felipe.ferreira65@fatec.sp.gov.br>
2022-02-17 22:00:55 -0300

    Cálculo da nota bimestral, condicionado a situação do aluno

---
+++ src/Estruturas_Condicionais/Ex04_NotasBimestrais.java
@@ -0,0 +1,26 @@
+package Estruturas_Condicionais;
+
+/**
+ * @author Felipe Castro Ferreira
+ */
+
+import javax.swing.JOptionPane;
+
+public class Ex04_NotasBimestrais {
+    public static void main(String[] args) {
+        double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Insira a primeira nota: "));
+        double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Insira a segunda nota: "));
+        double nota3 = Double.parseDouble(JOptionPane.showInputDialog("Insira a terceira nota: "));
+        double nota4 = Double.parseDouble(JOptionPane.showInputDialog("Insira a quarta nota: "));
+        
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        
+        if (media >= 6) {
+            JOptionPane.showMessageDialog(null, "Aprovado");
+        } else if (media >= 3) {
+            JOptionPane.showMessageDialog(null, "Exame");
+        } else {
+            JOptionPane.showMessageDialog(null, "Retido");
+        }
+    }
+}
