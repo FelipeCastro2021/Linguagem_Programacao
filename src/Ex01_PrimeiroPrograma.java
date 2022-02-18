1b5ce959f8f3eb0e45d107583cf4a80889f22b48
Felipe Castro Ferreira <felipe.ferreira65@fatec.sp.gov.br>
2022-02-17 21:40:55 -0300

    Primeiro Projeto - Usando o JOptionPane

---
+++ src/Ex01_PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+/**
+ * @author Felipe Castro Ferreira
+ */
+
+import javax.swing.JOptionPane;
+
+public class Ex01_PrimeiroPrograma {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Primeiro Programa");
+    }
+}
