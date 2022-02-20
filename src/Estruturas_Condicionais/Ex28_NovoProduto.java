f10236c29db69e815adb5db67d79b12e61fff256
Felipe Castro 2021 <felipe2021castro@gmail.com>
2022-02-20 02:43:59 -0300

    Estabelecimento de novo preço para produto

---
+++ src/Estruturas_Condicionais/Ex28_NovoProduto.java
@@ -0,0 +1,28 @@
+package Estruturas_Condicionais;
+
+import javax.swing.JOptionPane;
+
+/*
+ * @Author: Felipe Castro Ferreira
+ * */
+
+public class Ex28_NovoProduto {
+	public static void main(String[] args) {
+		double precoAtual, mediaMensal;
+		
+		precoAtual = Double.parseDouble(JOptionPane.showInputDialog("Informe o preço atual do produto: "));
+		mediaMensal = Double.parseDouble(JOptionPane.showInputDialog("Informe a média mensal do produto: "));
+		
+		if (mediaMensal < 500 && precoAtual < 30) {
+			precoAtual *= 1.10;
+		} else if (mediaMensal >= 500 && precoAtual < 80) {
+			precoAtual *= 1.15;
+		} else if (mediaMensal >= 1000 && precoAtual >= 80) {
+			precoAtual *= 0.95;
+		} else {
+			precoAtual = precoAtual;
+		}
+		
+		JOptionPane.showMessageDialog(null, "Novo preço do produto: " + precoAtual)	;
+	}
+}
