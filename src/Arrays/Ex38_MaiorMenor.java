5336723d27ac0d85b685f024241ccd03d3cbeeec
Felipe Castro 2021 <felipe2021castro@gmail.com>
2022-02-21 14:20:43 -0300

    Maior e menor número de um array de números

---
+++ src/Arrays/Ex38_MaiorMenor.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import javax.swing.JOptionPane;
+
+public class Ex38_MaiorMenor {
+	public static void main(String[] args) {
+		int[] numeros = new int[10];
+		int maior = 0, menor = 0;
+		
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+			
+			maior = numeros[0];
+			menor = numeros[0];
+			
+			if (numeros[i] > maior) {
+				maior = numeros[i];
+			}
+			if (numeros[i] < menor) {
+				menor = numeros[i];
+			}
+		}
+		
+		JOptionPane.showMessageDialog(null, "Maior número: " + maior 
+				+"\nMenor número: " + menor);
+	}
+}
--- src/Estruturas_Repetitivas/Ex35_SomatorioImpares.java
+++ src/Estruturas_Repetitivas/Ex35_SomatorioImpares.java
@@ -4,16 +4,14 @@ import javax.swing.JOptionPane;
 
 public class Ex35_SomatorioImpares {
 	public static void main(String[] args) {
-		int num1, num2, maior = 0, somatoria = 0;
+		int num1, num2, somatoria = 0;
 		
 		num1 = Integer.parseInt(JOptionPane.showInputDialog("Insira o 1º número: "));
 		num2 = Integer.parseInt(JOptionPane.showInputDialog("Insira o 2º número: "));
 		
 		if (num1 > num2) {
-			maior = num1;
 			JOptionPane.showMessageDialog(null, num1 + " é maior que: " + num2);
 		} else {
-			maior = num2;
 			JOptionPane.showMessageDialog(null, num2 + " é maior que: " + num1);
 		}
 		
