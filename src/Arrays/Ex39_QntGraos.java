1ba1c319f12c8c213da375ce6babd8b7ba9c28e9
Felipe Castro 2021 <felipe2021castro@gmail.com>
2022-02-24 13:46:08 -0300

    Modularização - Quantidade de Grão crescente por 2

--- src/Arrays/Ex39_QntGraos.java
+++ src/Arrays/Ex39_QntGraos.java
@@ -4,20 +4,22 @@ import javax.swing.JOptionPane;
 
 public class Ex39_QntGraos {
 	public static void main(String args[]) {
-		double[] casa = new double[64];
-		double[] quantidade = new double[64];
+		double[] casa = new double[65];
+		double[] quantidade = new double[65];
 		
-		for (int i = 1; i < casa.length; i++) {
-			casa[i] = i;
-			quantidade[i] = i;
+		for (int i = 0; i < casa.length - 1; i++) {
+			int j = i + 1;
 			
-			if (i > 1) {
-				quantidade[i] = quantidade[i-1] * 2;				
+			casa[j] = j;
+			quantidade[j] = j;
+			
+			if (j > 1) {
+				quantidade[j] = quantidade[j-1] * 2;				
 			}
 			
-			System.out.print(casa[i] + "");
+			System.out.print(casa[j] + "");
 			System.out.print("/");
-			System.out.print(quantidade[i] + " ");
+			System.out.print(quantidade[j] + " ");
 		}
 	}
 }
