afcd93e176249a74a9913a3de2f77e83d047ac4a
FelipeCastro2021 <felipe2021castro@gmail.com>
2022-03-10 21:17:30 -0300

    Consertando mensagem de inserção do Diagonal Matriz

---
+++ src/Vetores_Matrizes/Ex09_LinhaCentralMatriz.java
@@ -0,0 +1,27 @@
+package Vetores_Matrizes;
+
+import javax.swing.JOptionPane;
+
+/*
+ * @Author: Felipe Castro Ferreira
+ * */
+
+public class Ex09_LinhaCentralMatriz {
+	public static void main(String[] args) {
+		int[][] matriz = new int[4][4];
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				matriz[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+					
+				if (i == j) { 
+					System.out.print("[ " + matriz[i][j] + " ]");
+				} else {
+					System.out.print("[ ]");
+				}
+			}
+			
+			System.out.println();
+		}
+	}
+}
